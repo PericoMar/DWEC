dc84e9234eb435a77f7ff2518dd9d7b4e4e002a7
PericoMar <pedromartinez.22@campuscamara.es>
2023-11-16 18:55:07 +0100

    [Update] Finished Exercise Painting JS

--- practicas_js/Ejercicios_04/js/pintaCuadros.js
+++ practicas_js/Ejercicios_04/js/pintaCuadros.js
@@ -1,34 +1,35 @@
 let paleta = document.getElementById("paleta")
 let lienzo = document.getElementById("lienzo")
+const EJEX_LIENZO = 30;
+const EJEY_LIENZO = 40;
 
 const COLORES = ["blue", "green", "yellow", "pink", "red"];
 
 for(i = 0 ; i < COLORES.length ; i++){
     let divColor = document.createElement("div")
-    divColor.innerHTML = COLORES[i];
     divColor.style.backgroundColor = COLORES[i];
     divColor.id = COLORES[i];
     paleta.appendChild(divColor);
 }
 
-for(i = 1 ; i <= 4 ; i++){
+for(i = 1 ; i <= EJEX_LIENZO ; i++){
     let fila = document.createElement("div");
     lienzo.appendChild(fila);
     fila.id = "fila" + i;
-    for(j = 1 ; j <= 5 ; j++){
+    for(j = 1 ; j <= EJEY_LIENZO ; j++){
         let divPintar = document.createElement("div");
         divPintar.id = "casilla" + i + j;
-        divPintar.innerHTML = "casilla" + i + j;
-        divPintar.style.height = "100px";
-        divPintar.style.width = "100px";
-        divPintar.style.border = "5px solid black"
+        divPintar.style.height = "40px";
+        divPintar.style.width = "40px";
+        divPintar.style.border = "1px solid black"
         fila.appendChild(divPintar);
     }
     fila.style.display = "flex";
-    fila.style.justifyContent = "space-evenly";
+    fila.style.justifyContent = "center";
 }
 
 const selected = document.getElementById("selected");
+selected.style.border= "1px solid black";
 let color;
 let arrayDivs = Array.from(paleta.getElementsByTagName("div"));
 arrayDivs.forEach(div => {
@@ -37,4 +38,12 @@ arrayDivs.forEach(div => {
     })
 })
 
-let arrayDivsPintar = Array.form
+let arrayDivsPintar = Array.from(lienzo.getElementsByTagName("div"));
+arrayDivsPintar.forEach(div => {
+    casillasPintar = Array.from(div.getElementsByTagName("div"));
+    casillasPintar.forEach(casilla => {
+        casilla.addEventListener("click", () => {
+            casilla.style.backgroundColor = selected.style.backgroundColor;
+        })
+    })
+})
