2622350eb37917d0c66aa7df1d60a858db58b68b
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-02-01 23:04:22 +0100

    [Update] Search photos by input

--- practicas_js/buscador-imagenes/js/app.js
+++ practicas_js/buscador-imagenes/js/app.js
@@ -2,7 +2,7 @@ const API_KEY = 'nj6BKQ_waiRqB_2UduIIL2b_CehFMzX1UJVgzkNe1lg';
 
 const resultado = document.getElementById("resultado");
 
-cargarImagenes(2);
+cargarImagenes();
 
 const btnNext = document.getElementById("btn-next");
 const btnBack = document.getElementById("btn-back");
@@ -17,35 +17,40 @@ searchForm.addEventListener("submit", (ev) => {
     query = termino.value;
     resultado.innerHTML = "";
     page = 1;
-    cargarImagenes(page, query);
+    if(!query){
+        cargarImagenes(page);
+    } else {
+        cargarImagenesConQuery(page,query);
+    }
 });
 
 btnNext.addEventListener("click", () => {
     page++;
     resultado.innerHTML = "";
-    cargarImagenes(page,query);
+    if(!query){
+        cargarImagenes(page,query);
+    } else {
+        cargarImagenesConQuery(page,query);
+    }
     btnBack.disabled = false;
 });
 
 btnBack.addEventListener("click", () => {
     page--;
     resultado.innerHTML = "";
-    cargarImagenes(page, query);
-    if (page = 1) {
+    if(!query){
+        cargarImagenes(page,query);
+    } else {
+        cargarImagenesConQuery(page,query);
+    }
+    if (page === 1) {
         btnBack.disabled = true;
     }
 });
 
-async function cargarImagenes(page = 1, query) {
+async function cargarImagenes(page = 1) {
     try {
-
-        let apiUrl = `https://api.unsplash.com/photos?per_page=6&page=${page}&client_id=${API_KEY}`;
-
-        if (query) {
-            apiUrl += `&query=${query}`;
-        }
-
-        const response = await fetch(apiUrl);
+        const response = await fetch(`https://api.unsplash.com/photos?per_page=6&page=${page}&client_id=${API_KEY}`);
 
         if (!response.ok) {
             throw new Error(`Error al cargar las imágenes. Estado: ${response.status}`);
@@ -64,3 +69,26 @@ async function cargarImagenes(page = 1, query) {
         console.error("Error al cargar imágenes:", error);
     }
 }
+
+async function cargarImagenesConQuery(page = 1, query) {
+    try {
+        const response = await fetch(`https://api.unsplash.com/search/photos?per_page=6&page=${page}&query=${query}&client_id=${API_KEY}`);
+
+        if (!response.ok) {
+            throw new Error(`Error al cargar las imágenes. Estado: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        for (const json of data.results) {
+            const img = document.createElement("img");
+            img.src = json.urls.regular;
+            img.classList.add("imagen");  // Agregar una clase para aplicar estilos
+            resultado.appendChild(img);
+        }
+    } catch (error) {
+        console.error("Error al cargar imágenes:", error);
+    }
+}
+
