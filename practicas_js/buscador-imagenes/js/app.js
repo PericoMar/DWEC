6b1c3c2cd3822af44a5db698cb67e69c0b6646c5
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-02-01 12:49:26 +0100

    [Update] Photos showed on Web smaller but same resolution

--- practicas_js/buscador-imagenes/js/app.js
+++ practicas_js/buscador-imagenes/js/app.js
@@ -1,10 +1,14 @@
-fetch("https://api.unsplash.com/collections?client_id=nj6BKQ_waiRqB_2UduIIL2b_CehFMzX1UJVgzkNe1lg")
+fetch("https://api.unsplash.com/photos?page=2?client_id=nj6BKQ_waiRqB_2UduIIL2b_CehFMzX1UJVgzkNe1lg")
 .then(response => response.json())
 .then(data => {
     console.log(data);
     resultado = document.getElementById("resultado");
-    img = document.createElement("img");
-    img.src = data[0].links.html;
-    resultado.appendChild(img);  
+    data.forEach(json => {
+        var img = document.createElement("img");
+        img.src = json.urls.regular;
+        img.classList.add("imagen-pequena");  // Agregar una clase para aplicar estilos
+        resultado.appendChild(img);
+    });
+    
 });
 
